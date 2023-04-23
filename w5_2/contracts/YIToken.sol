bf02cd9932f33beee3307a66a153bfade4025b99
0xfa1e <125674197+0xfa1e@users.noreply.github.com>
2023-04-23 19:57:46 +0800

    Add files via upload

---
+++ w5_2/contracts/YIToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract YIToken is ERC20 {
+    constructor()  ERC20("YI", "YI"){
+        super._mint(msg.sender,100000 * 1000000000000000000);
+    }
+
+}
+
+// import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+// contract YIToken is ERC20Upgradeable {
+//     constructor() {
+//     }
+
+//     function initialize() initializer public {
+//         __ERC20_init("YI", "YI");
+//         super._mint(msg.sender,100000 * 1000000000000000000);
+//     }
+// } 
--- w5_2/contracts/YITokenMarket.sol
+++ w5_2/contracts/YITokenMarket.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "hardhat/console.sol";
 
 interface IUniswapV2Router02 {
+    function factory() external pure returns (address);
+    function WETH() external pure returns (address);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
     function addLiquidityETH(
         address token,
         uint amountTokenDesired,
@@ -11,14 +26,81 @@ interface IUniswapV2Router02 {
         address to,
         uint deadline
     ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountToken, uint amountETH);
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETHWithPermit(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountToken, uint amountETH);
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
     function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
         external
         payable
         returns (uint[] memory amounts);
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+
+    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
 }
 
 contract YITokenMarket {
-    IUniswapV2Router02 uniswapV2Router02;
+    using SafeERC20 for IERC20;
+    address uniswapV2Router02;
     address wETHaddress;
     address yiToken;
 
@@ -28,31 +110,29 @@ contract YITokenMarket {
         if(_yiToken == address(0) || _WETH == address(0) || _uniswapV2Router02 == address(0)) 
             revert InvaliedInitParam();
 
-        uniswapV2Router02 = IUniswapV2Router02(_uniswapV2Router02);
+        uniswapV2Router02 = _uniswapV2Router02;
         wETHaddress = _WETH;
         yiToken = _yiToken;
     }
 
     function addLiquidityETH(
-        uint amountToken
+        uint yiTokenAmount
     ) external payable{
-        ERC20 token = ERC20(yiToken);
-        token.transferFrom(msg.sender, address(this), amountToken);
-        token.approve(address(uniswapV2Router02), amountToken);
+        console.log("addLiquidityETH",yiTokenAmount,msg.value);
+        IERC20 token = IERC20(yiToken);
+        token.safeTransferFrom(msg.sender, address(this), yiTokenAmount);
+        token.safeApprove(uniswapV2Router02, yiTokenAmount);
+        console.log("uniswapV2Router02:",uniswapV2Router02);
 
-        uniswapV2Router02.addLiquidityETH{value: msg.value}(yiToken, amountToken, 0, 0, msg.sender, block.timestamp);
+        IUniswapV2Router02(uniswapV2Router02).addLiquidityETH{value: msg.value}(yiToken, yiTokenAmount, 100, 100, msg.sender, block.timestamp);
+        // IUniswapV2Router01(router).addLiquidityETH{value: msg.value}(myToken, tokenAmount, 0, 0, msg.sender, block.timestamp);
     }
 
-    function byToken(uint amountOutMin) external payable returns (uint amountToken) {
-        address[] path = new address[](2);
-        path[0] = WETH;
+    function byToken(uint amountOutMin) external payable{
+        address[] memory path = new address[](2);
+        path[0] = wETHaddress;
         path[1] = yiToken;
-
-        uint[] memory amounts = uniswapV2Router02.swapExactETHForTokens{value: msg.value}(amountOutMin, path, address(this), block.timestamp);
-        amountToken = amounts[amounts.length-1];
-
-        uint amount = ERC20(yiToken).balanceOf(address(this));
-        ERC20(yiToken).transfer(msg.sender, amount);
-
+        
+        IUniswapV2Router02(uniswapV2Router02).swapExactETHForTokens{value: msg.value}(amountOutMin, path, msg.sender, block.timestamp);
     }
 }
